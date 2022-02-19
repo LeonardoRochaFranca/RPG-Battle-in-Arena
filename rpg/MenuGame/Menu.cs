dfb8a3ec7a9d9d6244335dc0faedbc88fe043f1f
LeonardoRochaFranca <franca.leonardorocha@gmail.com>
2022-02-19 01:58:51 -0300

    Adjustment in game's Menu

--- rpg/MenuGame/Menu.cs
+++ rpg/MenuGame/Menu.cs
@@ -1,4 +1,5 @@
-﻿using rpg.Entities;
+﻿using rpg.Battle;
+using rpg.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -13,45 +14,32 @@ namespace rpg.MenuGame
         {
             Console.WriteLine("----------------------------------------------");
             Console.WriteLine("     Bem vind@ ao RPG 'Battle in Arena'!!!    ");
-            Console.WriteLine("     Escolha a classe do seu personagem 1:    ");
-            Console.WriteLine("     1 - Knight \n     2 - Wizard \n     3 - Ninja   ");
-            int opt1 = int.Parse(Console.ReadLine());
-
-            Console.Write("    Insira agora o nome do personagem 1: ");
+            Console.Write("       Escolha o nome do seu Guerreiro: ");
             string perso1 = Console.ReadLine();
 
-            CreateCharacter(opt1, perso1);
-
-            Console.WriteLine("  Escolha a classe do seu personagem 2:  ");
-            Console.Write("     1 - Knight \n     2 - Wizard \n     3 - Ninja   ");
-            int opt2 = int.Parse(Console.ReadLine());
-
-            Console.Write("    Insira agora o nome do personagem 1: ");
+            Console.Write("          Escolha o nome do seu Mago: ");
             string perso2 = Console.ReadLine();
 
-            Console.Write("  Insira agora o nome do seu adversário: ");
+            Console.Write("      Insira agora o nome do seu adversário: ");
             string bossName = Console.ReadLine();
 
-            Console.Write("Insira o nome da arena que você deseja batalhar:");
+            Console.Write("Insira o nível da arena que você deseja batalhar: ");
             int arenaLvl = int.Parse(Console.ReadLine());
+
+            SetBattle(perso1, perso2, bossName, arenaLvl);
         }
-        public void CreateCharacter(int opt, string perso, object p)
+        public void SetBattle(string perso, string perso2, string bossName, int arenaLvl)
         {
 
-            if (opt == 1)
-            {
-                object p = new Wizard(perso);
-            }
-            if (opt == 2)
-            {
-                object p = new Wizard(perso);
-            }
-            if (opt == 3)
-            {
-                object p = new Ninja(perso);
-            }
+            Knight p1 = new Knight(perso);
+            Wizard p2 = new Wizard(perso2);
+            Boss boss = new Boss(bossName);
+            Arena arena = new Arena(arenaLvl);
+            boss.ArenaBoss(arena.ArenaLevel);
+
+            arena.Battle(p1, p2, boss);
 
         }
     }
 }
-}
+
--- rpg/Program.cs
+++ rpg/Program.cs
@@ -11,13 +11,6 @@ namespace rpg
         {
             var menu = new Menu();
             menu.Show();
-            //Knight p1 = new Knight("Don");
-            //Ninja p2 = new Ninja("Safira");
-            //Boss boss = new Boss("Monster");
-            //Arena arena = new Arena(1);
-            //boss.ArenaBoss(arena.ArenaLevel);
-
-            //arena.Battle(p1, p2, boss);
 
         }
     }
